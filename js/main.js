3d216e996af56497af46da50dd2bbe223c8b2fec
swapnil2411 <67681257+swapnil2411@users.noreply.github.com>
2022-04-09 14:44:37 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// mobile Navbar
+
+$('.hamburger').on('click', function(){
+    $('.mobile_navbar').addClass('active');
+})
+
+$('.close_mobile_menu').on('click', function(){
+    $('.mobile_navbar').removeClass('active');
+})
+
+// search visible
+
+$('.mobile_search .btn').on('click', function(){
+    $('.top_header_right').addClass('active');
+})
+
+
+$('.close_search_box').on('click', function(){
+    $('.top_header_right').removeClass('active');
+})
+
+.close_search_box
+
+// Banner Slider
+$('#banner_slider').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    items:1
+})
+
+//Testimonial Slider
+$('#testimonial_slider').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    items:1
+})
+
+//Tab Content
+$('.tab_panels .tabs li').on('click', function(){
+    $(this).addClass('active').siblings().removeClass('active');
+    var panelToShow = $(this).attr('rel');
+    $('.tab_panels .panel.active').fadeOut(300, function(){
+        $(this).removeClass('active');
+        $('#' + panelToShow).fadeIn(300, function(){
+            $(this).addClass('active');
+        })
+    })
+})
